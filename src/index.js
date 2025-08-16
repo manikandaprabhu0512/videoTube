8b60fa3febc1b2403404d132b2f9a50e11604c01
mmanikandap0512 <mmanikandap0512@gmail.com>
2025-08-16 16:36:17 +0530

    Added Utils Components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-connectDB();
+connectDB()
+  .then(
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server Running on Port ${process.env.PORT}`);
+    })
+  )
+  .catch((err) => {
+    console.log("Mongo DB Connection Error: ", err);
+  });
 
 /*
 import express from "express";
