a8e75d0f1e36d5a3b1b9c02b7b092230e44a23e5
mmanikandap0512 <mmanikandap0512@gmail.com>
2025-09-06 23:48:20 +0530

    fix: add missing file extensions to import statements in healthcheck controller

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -1,5 +1,5 @@
-import ApiResponse from "../utils/ApiResponse";
-import asyncHandler from "../utils/asyncHandler";
+import ApiResponse from "../utils/ApiResponse.js";
+import asyncHandler from "../utils/asyncHandler.js";
 
 const healthcheck = asyncHandler(async (req, res, next) => {
   return res.status(200).json(new ApiResponse(200, {}, "ok"));
