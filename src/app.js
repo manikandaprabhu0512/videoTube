4a9b3487eded97fc5bf3242a422364cb027f8b74
mmanikandap0512 <mmanikandap0512@gmail.com>
2025-09-09 00:55:29 +0530

    CORS update

--- src/app.js
+++ src/app.js
@@ -6,7 +6,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CORS_ORGIN,
+    // origin: process.env.CORS_ORGIN,
+    origin: "http://localhost:5173/",
     credentials: true,
   })
 );
